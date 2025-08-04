69d1be681acf5d234e8eb5fc8811bdeb8c3a0b7f
DaveInnovator <145847850+DaveInnovator@users.noreply.github.com>
2025-08-04 21:50:44 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,17 +10,18 @@ export default function Hero() {
           transition={{ duration: 0.8 }}
         >
           <p className="mb-4 inline-block bg-blue-700/20 px-3 py-1 rounded-full text-xs text-blue-300 font-semibold border border-blue-400">
-            NEW &nbsp;•&nbsp; Trusted by Modern SaaS Brands
+            NEW &nbsp;•&nbsp; Trusted by Modern  Brands
           </p>
 
           <h1 className="text-3xl sm:text-4xl font-bold leading-tight mb-4">
             Scalable Web Solutions <br />
-            <span className="text-blue-400">for SaaS & High-Growth Brands</span>
+            <span className="text-blue-400">for personal brands and creators</span>
           </h1>
 
           <p className="text-base sm:text-lg text-gray-300 mb-8">
-            At NexaForge, we design and develop lightning-fast, conversion-focused websites that empower SaaS platforms and modern businesses to grow, scale, and dominate their market.
-          </p>
+            At NexaForge, we build bold, conversion-driven websites, landing pages, and tools that help creators,coaches and personal brands monetize fast, look legit brand. and scale
+            
+            </p>
 
           <div className="flex flex-wrap justify-center gap-4">
             <Link
